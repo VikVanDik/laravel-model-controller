8c9ed58b162c9dd7f7c4f24eeaf6b27029ebce83
VikVanDik <95vlc17@gmail.com>
2023-11-20 16:55:16 +0100

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+@extends("layouts.main")
+
+@section("content")
+
+<h1>My movies</h1>
+
+<div class="container d-flex flex-wrap">
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">TITOLO FILM</h5>
+          <p class="card-text">Nazionalità</p>
+          <p class="card-text">Voto</p>
+          <p class="card-text">Data</p>
+
+        </div>
+      </div>
+</div>
+
+@endsection
